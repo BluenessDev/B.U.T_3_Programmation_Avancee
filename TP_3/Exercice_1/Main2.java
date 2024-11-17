e38cd4c86044a9c576565835abf9c8a3c9990b71
Cyril TILAN <cyril.tilan@gmail.com>
2024-11-17 20:11:22 +0100

    A : Ajout du I2 à l'exercice 1 avec un producteur attendant l'input d'un utilisateur

---
+++ TP_3/Exercice_1/Main2.java
@@ -0,0 +1,23 @@
+package TP_3.Exercice_1;
+
+public class Main2 {
+    public static void main(String[] args) {
+        int nbMessages = 5; // Nombre de buffers
+        BAL bal = new BAL(nbMessages);
+
+        Thread producteur = new Thread(new ProducteurClavier(bal, nbMessages));
+        Thread consommateur = new Thread(new Consommateur(bal, nbMessages));
+
+        producteur.start();
+        consommateur.start();
+
+        try {
+            producteur.join();
+            consommateur.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Fin du programme.");
+    }
+}
---
+++ TP_3/Exercice_1/ProducteurClavier.java
@@ -0,0 +1,42 @@
+package TP_3.Exercice_1;
+
+import java.util.Scanner;
+
+import static java.lang.Thread.sleep;
+
+public class ProducteurClavier implements Runnable {
+    private final BAL bal;
+    private final int nbMessages;
+
+    public ProducteurClavier(BAL bal, int nbMessages) {
+        this.bal = bal;
+        this.nbMessages = nbMessages;
+    }
+
+    @Override
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            int i = 0;
+            while (true) {
+                sleep((long) (Math.random() * 500));
+                System.out.print("Entrez une lettre (ou 'Q' pour quitter) : ");
+                String lettre = scanner.nextLine();
+                if (lettre.length() != 1) {
+                    System.out.println("Veuillez entrer une seule lettre.");
+                    continue;
+                }
+                bal.write(i % nbMessages, lettre); // Écriture dans le buffer
+                System.out.println("Produit: " + lettre + " dans le buffer " + (i % nbMessages));
+                i++;
+                if ("Q".equals(lettre)) { // Arrêt si 'Q' est saisi
+                    break;
+                }
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } finally {
+            scanner.close();
+        }
+    }
+}
